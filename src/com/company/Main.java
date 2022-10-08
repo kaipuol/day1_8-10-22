3fdbcd0011f17ae2cc7a0149f5511dac5ffef2fd
kaipuol <dloupiak@mail.ru>
2022-10-08 19:16:47 +0300

    init project

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b=20, c=3;
+        a=b+c;
+        System.out.println(a);
+
+        a=2; c=4;
+        if (a>0 & ++c>0) {}
+        System.out.println(c);
+    }
+}
